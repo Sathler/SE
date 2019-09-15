54d0913d0603d765532f01ff309866a241f32d66
Sathler <Vinicius.sathler3@yahoo.com.br>
2019-09-15 10:24:02 -0300

    tarefa-02-adc

---
+++ tarefa-02/tarefa2-v2.ino
@@ -0,0 +1,54 @@
+#include "SevSeg.h"
+
+#define flexSensor A0
+#define IRsensor 11
+
+SevSeg sevseg;
+
+void estado1(){
+  sevseg.blank();
+}
+
+void estado2(){
+  ADCSRA |= (1 << ADSC);
+  while(ADCSRA & (1 << ADSC));
+  int flex = ADCL;
+  flex += (ADCH << 8);
+  //int flex = analogRead(flexSensor);
+  Serial.println(flex);
+  flex = map(flex, 400, 1000, 0, 9);
+  sevseg.setNumber(flex);
+  sevseg.refreshDisplay();
+}
+
+void setup() {
+   byte numDigits = 1;
+   byte digitPins[] = {};
+   byte segmentPins[] = {6, 5, 2, 3, 4, 7, 8, 9};
+   bool resistorsOnSegments = true;
+   byte hardwareConfig = COMMON_CATHODE; 
+   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+   sevseg.setBrightness(90);
+   sevseg.setNumber(0);
+   sevseg.refreshDisplay();
+
+   pinMode(IRsensor, INPUT);
+    //analogic read setup
+    ADMUX = 0;
+    ADMUX |= (1 << REFS0);
+    ADCSRA |= (1 << ADEN);
+  
+  Serial.begin(9600); 
+
+}
+
+void loop() {
+  int estado = digitalRead(IRsensor);
+  
+  if(!estado){ 
+    estado1();
+  }
+  else {
+    estado2();
+  }
+}
